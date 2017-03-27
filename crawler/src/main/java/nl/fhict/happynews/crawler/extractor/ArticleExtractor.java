f553e83c4328e8a893ba3ab0ab8e499a1b0fe74b
Sven Dubbeld <sven.dubbeld1@gmail.com>
2017-03-27 15:56:44 +0200

    Extract content

---
+++ crawler/src/main/java/nl/fhict/happynews/crawler/extractor/ArticleExtractor.java
@@ -0,0 +1,64 @@
+package nl.fhict.happynews.crawler.extractor;
+
+import nl.fhict.happynews.shared.Post;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * An extractor for the body of an article.
+ */
+@Service
+public class ArticleExtractor implements ContentExtractor {
+
+    private final Logger logger = LoggerFactory.getLogger(ArticleExtractor.class);
+
+    private RestTemplate restTemplate = new RestTemplate();
+    private HttpEntity entity;
+    private Whitelist whitelist = new Whitelist();
+
+    private String apiPrefix = "https://mercury.postlight.com/parser?url=";
+    private String apiKey = "fbfOQzeyQ0aXewj3oxJQ1D9zP24QuoBAxejPadhr";
+
+    public ArticleExtractor() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("x-api-key", apiKey);
+
+        entity = new HttpEntity(headers);
+    }
+
+    @Override
+    public String extract(Post post) {
+        StringBuilder builder = new StringBuilder();
+
+        String nextUrl = apiPrefix + post.getUrl();
+        try {
+            MercuryResponse response;
+            do {
+                response = restTemplate.exchange(nextUrl, HttpMethod.GET, entity, MercuryResponse.class).getBody();
+
+                builder.append(Jsoup.clean(response.getContent(), whitelist));
+
+                nextUrl = apiPrefix + response.getNextPageUrl();
+            } while (response.getNextPageUrl() != null);
+
+        } catch (RestClientException e) {
+            logger.warn("Could not read from " + nextUrl, e);
+        }
+
+        String result = builder.toString();
+
+        if (result.isEmpty()) {
+            return post.getContentText();
+        } else {
+            return result;
+        }
+    }
+}
