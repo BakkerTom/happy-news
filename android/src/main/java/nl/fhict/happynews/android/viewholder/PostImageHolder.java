c5faf604192442e8d9d3b82c64047bf9f009a293
Bakker,Tom T.A.C <tac.bakker@student.fontys.nl>
2017-04-10 15:14:35 +0200

    Added Spinner to image loading

--- android/src/main/java/nl/fhict/happynews/android/viewholder/PostImageHolder.java
+++ android/src/main/java/nl/fhict/happynews/android/viewholder/PostImageHolder.java
@@ -7,8 +7,10 @@ import android.support.v7.widget.RecyclerView;
 import android.text.format.DateUtils;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 import nl.fhict.happynews.android.model.Post;
 import nl.fhict.happynews.android.R;
@@ -24,6 +26,7 @@ public class PostImageHolder extends ViewHolder{
     private TextView timeTextView;
     private TextView headlineTextView;
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     public PostImageHolder(View view) {
         super(view);
@@ -33,17 +36,26 @@ public class PostImageHolder extends ViewHolder{
         timeTextView = (TextView) view.findViewById(R.id.timeTextView);
         headlineTextView = (TextView) view.findViewById(R.id.headlineTextView);
         imageView = (ImageView) view.findViewById(R.id.imageView);
+        progressBar = (ProgressBar) view.findViewById(R.id.spinner);
     }
 
     public void bindType(Post post) {
         super.bindType(post);
+
+        progressBar.setVisibility(View.VISIBLE);
 
         sourceTextView.setText(post.getSource());
         timeTextView.setText(relativeTimeSpan(post.getPublishedAt()));
         headlineTextView.setText(post.getTitle());
 
         Ion.with(imageView)
-                .load(post.getImageUrls().get(0));
+                .load(post.getImageUrls().get(0))
+                .setCallback(new FutureCallback<ImageView>() {
+                    @Override
+                    public void onCompleted(Exception e, ImageView result) {
+                        progressBar.setVisibility(View.GONE);
+                    }
+                });
     }
 
 }
