46755fd5f2601841dcee3967ac37ef7cba9dde2c
DaanTul <daantuller94@gmail.com>
2017-05-08 10:55:22 +0200

    Added notification receiver

---
+++ android/src/main/java/nl/fhict/happynews/android/receiver/NotificationReceiver.java
@@ -0,0 +1,42 @@
+package nl.fhict.happynews.android.receiver;
+
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.SystemClock;
+import android.support.v7.app.NotificationCompat;
+import android.util.Log;
+import nl.fhict.happynews.android.R;
+
+/**
+ * Created by daan_ on 8-5-2017.
+ */
+public class NotificationReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            // Set the alarm here.
+            AlarmManager alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+            Intent i = new Intent(context, NotificationReceiver.class);
+            PendingIntent alarmIntent;
+            alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+            alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + 10000, alarmIntent);
+        }
+
+        NotificationManager notificationManager;
+        notificationManager = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+        Notification notification = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.articlebadge)
+                .setContentTitle("My notification")
+                .setContentText("Hello World!").build();
+
+        notificationManager.notify(R.string.app_name, notification);
+    }
+}
