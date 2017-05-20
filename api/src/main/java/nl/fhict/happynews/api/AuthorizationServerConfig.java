d290050f0905bfb26d021fa2dc7b1c3e25c59ab2
Bakker,Tom T.A.C <tac.bakker@student.fontys.nl>
2017-05-20 11:13:47 +0200

    Added configuration to WebSecurityConfig

--- api/src/main/java/nl/fhict/happynews/api/AuthorizationServerConfig.java
+++ api/src/main/java/nl/fhict/happynews/api/AuthorizationServerConfig.java
@@ -7,11 +7,13 @@ import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.web.cors.CorsUtils;
 
 /**
  * Configuration for the OAuth2 server.
@@ -54,4 +56,5 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .authorizedGrantTypes("password", "authorization_code", "refresh_token", "implicit")
             .secret(clientSecret);
     }
+
 }
